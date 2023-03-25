b00bf8c926d982c923f44645cf0c2f8616ce65d1
parag1627 <paragpatil973047@gmail.com>
2023-03-25 21:47:02 +0530

    Added

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -1,6 +1,8 @@
 package com.driver.services.impl;
 
+import com.driver.model.Payment;
 import com.driver.model.PaymentMode;
+import com.driver.model.Reservation;
 import com.driver.repository.PaymentRepository;
 import com.driver.repository.ReservationRepository;
 import com.driver.services.PaymentService;
@@ -15,7 +17,35 @@ public class PaymentServiceImpl implements PaymentService {
     PaymentRepository paymentRepository2;
 
     @Override
-    public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
+    public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception
+    {
 
+        PaymentMode paymentMode;
+        if(mode.equalsIgnoreCase("cash"))
+            paymentMode = PaymentMode.CASH;
+        else if(mode.equalsIgnoreCase("upi"))
+            paymentMode = PaymentMode.UPI;
+        else if(mode.equalsIgnoreCase("card"))
+            paymentMode = PaymentMode.CARD;
+        else
+            throw new Exception("Payment mode not detected");
+
+        Payment payment = new Payment();
+        payment.setPaymentMode(paymentMode);
+        payment.setPaymentCompleted(true);
+
+        Reservation reservation = reservationRepository2.findById(reservationId).get();
+        int bill = reservation.getSpot().getPricePerHour() * reservation.getNumberOfHours();
+        if(amountSent < bill)
+            throw new Exception("Insufficient Amount");
+
+
+        payment.setReservation(reservation);
+        reservation.setPayment(payment);
+
+//        paymentRepository2.save(payment);
+        reservationRepository2.save(reservation);
+
+        return payment;
     }
 }
