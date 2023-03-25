63de2fa03927903d10da740a10e0780f9b64c659
parag1627 <paragpatil973047@gmail.com>
2023-03-25 22:31:51 +0530

    POP

--- src/main/java/com/driver/model/Payment.java
+++ src/main/java/com/driver/model/Payment.java
@@ -1,26 +1,89 @@
+////
+//// Source code recreated from a .class file by IntelliJ IDEA
+//// (powered by FernFlower decompiler)
+////
 //
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
+//package com.driver.model;
 //
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.JoinColumn;
+//import javax.persistence.OneToOne;
+//
+//@Entity
+//public class Payment {
+//    @Id
+//    @GeneratedValue(
+//            strategy = GenerationType.IDENTITY
+//    )
+//    private int id;
+//    private boolean paymentCompleted;
+//    private PaymentMode paymentMode;
+//    @OneToOne
+//    @JoinColumn
+//    private Reservation reservation;
+//
+//    public Payment() {
+//    }
+//
+//    public Payment(int id, Boolean paymentCompleted, PaymentMode paymentMode, Reservation reservation) {
+//        this.id = id;
+//        this.paymentCompleted = paymentCompleted;
+//        this.paymentMode = paymentMode;
+//        this.reservation = reservation;
+//    }
+//
+//    public int getId() {
+//        return this.id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public Boolean getPaymentCompleted() {
+//        return this.paymentCompleted;
+//    }
+//
+//    public void setPaymentCompleted(boolean paymentCompleted) {
+//        this.paymentCompleted = paymentCompleted;
+//    }
+//
+//    public PaymentMode getPaymentMode() {
+//        return this.paymentMode;
+//    }
+//
+//    public void setPaymentMode(PaymentMode paymentMode) {
+//        this.paymentMode = paymentMode;
+//    }
+//
+//    public Reservation getReservation() {
+//        return this.reservation;
+//    }
+//
+//    public void setReservation(Reservation reservation) {
+//        this.reservation = reservation;
+//    }
+//}
+
 
 package com.driver.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 @Entity
 public class Payment {
-    @Id
-    @GeneratedValue(
-            strategy = GenerationType.IDENTITY
-    )
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private boolean paymentCompleted;
+
+    @Enumerated(EnumType.STRING)
     private PaymentMode paymentMode;
+
     @OneToOne
     @JoinColumn
     private Reservation reservation;
@@ -28,7 +91,7 @@ public class Payment {
     public Payment() {
     }
 
-    public Payment(int id, Boolean paymentCompleted, PaymentMode paymentMode, Reservation reservation) {
+    public Payment(int id, boolean paymentCompleted, PaymentMode paymentMode, Reservation reservation) {
         this.id = id;
         this.paymentCompleted = paymentCompleted;
         this.paymentMode = paymentMode;
@@ -36,15 +99,15 @@ public class Payment {
     }
 
     public int getId() {
-        return this.id;
+        return id;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public Boolean getPaymentCompleted() {
-        return this.paymentCompleted;
+    public boolean isPaymentCompleted() {
+        return paymentCompleted;
     }
 
     public void setPaymentCompleted(boolean paymentCompleted) {
@@ -52,7 +115,7 @@ public class Payment {
     }
 
     public PaymentMode getPaymentMode() {
-        return this.paymentMode;
+        return paymentMode;
     }
 
     public void setPaymentMode(PaymentMode paymentMode) {
@@ -60,7 +123,7 @@ public class Payment {
     }
 
     public Reservation getReservation() {
-        return this.reservation;
+        return reservation;
     }
 
     public void setReservation(Reservation reservation) {
